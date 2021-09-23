93d67dcf9e4a4075ccae50c109b7af5d0f73848d
Jason Wang <jason.b.wang@avanade.com>
2021-09-23 14:23:48 +0800

    AppShell Long press issue

---
+++ MyTestApp/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using MyTestApp.ViewModels;
+using MyTestApp.Views;
+using Xamarin.Forms;
+
+namespace MyTestApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(ItemsPage), typeof(ItemsPage));
+        }
+
+    }
+}
---
+++ MyTestApp/ViewModels/AboutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+using MyTestApp.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MyTestApp.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Shell.Current.GoToAsync($"{nameof(ItemsPage)}"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
